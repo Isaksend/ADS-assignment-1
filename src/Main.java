b3a840a7420950b0459e050ca12edf738378f97f
Isakzend <islamkadyrbekov31@gmail.com>
2024-04-06 10:47:52 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,39 @@
+import java.util.Scanner;
 public class Main {
 
     public static int findMin(int[] array, int n){
-        if (n==1){
+        if (n==1) {
             return array[0];
         }
+
         return Math.min(array[n-1], findMin(array,n -1));
     }
-    public static void main(String[] args){
-        int[] numbers = {10,32,1,3,45};
+//    public static int averNum(int[] array, int n){
+//
+//    }
 
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inpur the number of elements in massive:");
+        int n = scanner.nextInt();
+        if (n<=0){
+            System.out.println("The number of elements in massive must be positive!");
+            scanner.close();
+            return;
+        }
+        int[] numbers = new int[n];
+        System.out.println("Input the "+ n + " number value:");
+        for(int i = 0; i < n; i++){
+            numbers[i] = scanner.nextInt();
+        }
+        scanner.close();
         int min = findMin(numbers,numbers.length);
         System.out.println("Minimun number: "+ min);
     }
-    
+
+
+
+
+
 }
